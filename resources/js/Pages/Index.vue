81f4b2d5b1a8fa5db32cb67a056b3b03d745e5bc
Jane <kitkatdonat@gmail.com>
2023-09-04 21:02:39 +0800

    Remove paragraph

--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -34,14 +34,6 @@ const isUserLoggedIn = computed(() => usePage().props.auth.user);
             <div class="row justify-content-center">
                 <div class="col-xl-8">
                     <h2>Welcome to REGINA VILLE 2000 HOA</h2>
-                    <p>
-                        Ut velit est quam dolor ad a aliquid qui aliquid. Sequi
-                        ea ut et est quaerat sequi nihil ut aliquam. Occaecati
-                        alias dolorem mollitia ut. Similique ea voluptatem. Esse
-                        doloremque accusamus repellendus deleniti vel. Minus et
-                        tempore modi architecto.
-                    </p>
-                    <a href="" class="btn-get-started">Read More</a>
                 </div>
             </div>
         </div>
